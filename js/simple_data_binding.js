e940c131b87ff2b6685ccdd1de926bc558e866db
Goory al Hamed <gooryalhamed@hotmail.com>
2017-03-02 00:22:58 +0300

    the solution

--- js/simple_data_binding.js
+++ js/simple_data_binding.js
@@ -1,3 +1,12 @@
 $(document).ready(function() {
-  
+  $('#textArea').on('keyup',function(event){
+  	var str = $(this).val();
+    var char = str.slice(-1);
+  	 if (event.keyCode != 8){
+  		$('#myDiv').append(char);
+  	}else{
+  		var div_str = $('#myDiv').text();
+      $('#myDiv').text(div_str.slice(0,-1));
+  	}
+  });
 });
